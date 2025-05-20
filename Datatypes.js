fe8791e4318cc15908af4a359bed7a56f85ea997
Himank-Mathur <himank.mathur@deeporion.com>
2025-05-20 05:50:05 +0530

    19 mai

---
+++ Datatypes.js
@@ -0,0 +1,26 @@
+//Data Types
+//19/05/2025
+
+"use Strict";
+
+console.log(3 + 3);
+console.log('Himank');
+
+let name= "himank";
+let age = 22;   
+let isLoggedIn = false
+
+// Number => 2 to power 53
+// bigint
+// string =>""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// Symbol => unique
+
+// object
+
+console.log(typeof "himank"); //string
+console.log(typeof age); //number
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
---
+++ script.js
@@ -0,0 +1,17 @@
+//let , var, const
+// 19/05/2025
+
+const accountId = 12345
+let accountEmail = "himank@getMaxListeners.com"
+var accountPassword = "12345"
+let accountCity = "jaipur"
+
+// accountId = 2; //not allowed
+
+accountEmail = "mathur@gmail.com"
+accountPassword = "10987653"
+accountCity = " jalore"
+
+console.log(accountId);
+console.table([accountId,accountCity, accountEmail, accountPassword]);
+ 

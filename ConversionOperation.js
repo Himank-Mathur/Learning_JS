1f64f795ef1b895cca4e774c1704588b06ba81ce
Himank-Mathur <himank.mathur@deeporion.com>
2025-05-20 08:34:39 +0530

    20/05/2025

---
+++ ConversionOperation.js
@@ -0,0 +1,53 @@
+// ConversionOperatin
+// 20/05/2025
+
+let score = 33
+let sco = "33"
+let scores = "33abc"
+let sc = null
+let scor = undefined
+let scoree = true
+
+console.log(typeof (score));
+console.log(typeof (sco));
+
+// let valueInNumber = Number(sco);
+// console.log(typeof (valueInNumber));
+
+// let valueInNumber = Number(scores);
+// console.log(typeof (valueInNumber));
+// console.log(valueInNumber);
+
+// let valueInNumber = Number(scor);
+// console.log(typeof (valueInNumber));
+// console.log(valueInNumber);
+
+let valueInNumber = Number(scoree);
+console.log(typeof (valueInNumber));
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+// let isLoggedIn = 1
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// let isLoggedIn = ""
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+let isLoggedIn = "Himank"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "himank" => true
+
+let someNum = 33
+let stringNum = String(someNum)
+console.log(stringNum);
+console.log(typeof(stringNum));
+ 
